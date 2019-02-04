9e37fd0c3182fad961b09da9d856fcedb9c9f8d2
constant1nos <dinobatzio@gmail.com>
2019-02-04 20:02:25 +0200

    test commit

--- myMovies/src/mymovies/MyMovies.java
+++ myMovies/src/mymovies/MyMovies.java
@@ -2,6 +2,7 @@
  * Created by Batzonis Constantinos
  */
 package mymovies;
+import GUI.MainMenu;
 
 /**
  *
@@ -13,7 +14,7 @@ public class MyMovies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MainMenu().setVisible(true);
     }
     
 }
