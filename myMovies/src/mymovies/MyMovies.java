2d68ff462ba740739c4cb6e328b8e3a0c2bfaa62
constant1nos <dinobatzio@gmail.com>
2019-02-09 10:37:35 +0200

    project.properties modified (from NetBeans)

--- myMovies/src/mymovies/MyMovies.java
+++ myMovies/src/mymovies/MyMovies.java
@@ -17,6 +17,5 @@ public class MyMovies {
         new MainMenu().setVisible(true);
         System.out.println("Test commit");
         System.out.println("κλιανησ χρηστοσ 07-02-2019");
-        // giorgos test
     }    
 }
