52479ea9d9eee5ee8cfeeaa378b8d8531ff46777
Batzonis Konstantinos <31585061+constant1nos@users.noreply.github.com>
2019-03-01 19:22:59 +0200

    Update MyMovies.java
    Παράδειγμα επίλυσης conflict

--- myMovies/src/mymovies/MyMovies.java
+++ myMovies/src/mymovies/MyMovies.java
@@ -21,7 +21,7 @@ public class MyMovies {
      */
     public static void main(String[] args) {
         
-        // Αρχικοποίηση EntityManagerFactory και EntityManager
+        // Δοκιμή επίλυσης conflict στο NetBeans...
         DBManager dbManager = DBManager.getInstance();
         
         /* Set Nimbus Look and Feel */
