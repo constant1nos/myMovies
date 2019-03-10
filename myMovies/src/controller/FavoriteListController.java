7a3065430e8e861c32d1a02b962df388a4f0caf4
Batzonis Konstantinos <31585061+constant1nos@users.noreply.github.com>
2019-03-10 18:06:09 +0200

    Προσθήκη και διόρθωση σχολίων

--- myMovies/src/controller/FavoriteListController.java
+++ myMovies/src/controller/FavoriteListController.java
@@ -1,5 +1,5 @@
 /*
- * Κλιάνης Χρήστος - Μπατζώνης Κωνσταντίνος - Σερβοζλίδης Γιώργος - Χαντζή Στεφανία
+ * Κλιάνης Χρήστος - Μπατζώνης Κωνσταντίνος - Σεβοζλίδης Γιώργος - Χαντζή Στεφανία
  */
 package controller;
 
@@ -9,7 +9,7 @@ import javax.persistence.Query;
 
 /**
  *
- * @author dinob
+ * @author Κλιάνης Χρήστος - Μπατζώνης Κωνσταντίνος - Σεβοζλίδης Γιώργος - Χαντζή Στεφανία
  */
 public class FavoriteListController extends MainController{
     
@@ -17,12 +17,14 @@ public class FavoriteListController extends MainController{
         super();
     } 
     
+    /* Αποθήκευση αγαπημένεης λίστας στη βάση δεδομένων */
     public void storeFavoriteToDataBase(FavoriteList favorite){
         em.getTransaction().begin();
         em.persist(favorite);
         em.getTransaction().commit();
     }
  
+    /* Διαγραφή αγαπημένεης λίστας από τη βάση δεδομένων */
     public void deleteFavoriteFromDataBase(String listName){
         em.getTransaction().begin();
         Query query = em.createNamedQuery("FavoriteList.deleteList");
@@ -30,18 +32,21 @@ public class FavoriteListController extends MainController{
         query.executeUpdate();
         em.getTransaction().commit();
     }
-   
+
+    /* Αντικατάσταση ονόματος αγαπημένης λίστας */
     public void updateFavoriteListName(FavoriteList favoriteList, String name){
         em.getTransaction().begin();
         favoriteList.setName(name);
         em.getTransaction().commit();
     }
-    
+   
+    /* Εύρεση όλων των αγαπημένων λιστών */    
     public List getFavoriteList(){
         Query query = em.createNamedQuery("FavoriteList.findAll");
         return query.getResultList();
     }
-    
+  
+    /* Εύρεση αγαπημένης λίστας με βάση το όνομα */      
     public FavoriteList getFavoriteListByName(String name){
         Query q = em.createNamedQuery("FavoriteList.findByName");
         q.setParameter("name", name);
